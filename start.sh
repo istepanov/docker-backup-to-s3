1e09e60178252d158153efdee9ddd37659b10f2d
Ilya Stepanov <dev@ilyastepanov.com>
2014-11-30 13:02:57 +0300

    Added missing trailing slash

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
 SECRET_KEY=${SECRET_KEY:?"SECRET_KEY env variable is required"}
 S3_PATH=${S3_PATH:?"S3_PATH env variable is required"}
-DATA_PATH=${DATA_PATH:-/data}
+DATA_PATH=${DATA_PATH:-/data/}
 
 echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
