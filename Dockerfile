386d1c4873b948a759a04333be30fdfab1a5ac21
Ilya Stepanov <dev@ilyastepanov.com>
2014-11-30 12:34:43 +0300

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:14.04
+
+RUN apt-get update && \
+    apt-get install -y s3cmd cron && \
+    rm -rf /var/lib/apt/lists/*
+
+ADD s3cfg /root/.s3cfg
+
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
+
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
+SECRET_KEY=${SECRET_KEY:?"SECRET_KEY env variable is required"}
+S3_PATH=${S3_PATH:?"S3_PATH env variable is required"}
+DATA_PATH=${DATA_PATH:-/data}
+
+echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
+echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
+
+if [[ "$1" == 'no-cron' ]]; then
+    exec /usr/bin/s3cmd sync "$DATA_PATH" "$S3_PATH"
+else
+    echo "0 12 * * * /usr/bin/s3cmd sync \"$DATA_PATH\" \"$S3_PATH\"" | crontab -
+    exec cron -f
+fi
