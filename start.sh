7eef9363d683bfa4bac8e6353d8da4d7e80895ba
Ilya Stepanov <dev@ilyastepanov.com>
2016-03-24 23:50:51 -0700

    Fix export DATA_PATH when run without cron

--- start.sh
+++ start.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
-SECRET_KEY=${SECRET_KEY:?"SECRET_KEY env variable is required"}
-S3_PATH=${S3_PATH:?"S3_PATH env variable is required"}
-DATA_PATH=${DATA_PATH:-/data/}
+: ${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
+: ${SECRET_KEY:?"SECRET_KEY env variable is required"}
+: ${S3_PATH:?"S3_PATH env variable is required"}
+export DATA_PATH=${DATA_PATH:-/data/}
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
-PARAMS=${PARAMS}
 
 echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
