d051664d81dff2fc8f26777099d547c40f1ccbb1
Marcello de Sales <Marcello_DeSales@intuit.com>
2017-03-14 01:30:17 -0700

    Updating start.sh: Support to git + volume: iwaitnotify instead of poll
    This commit adds support for iwaitnotify. That is, wait on files to be
    added to DATA_PATH. That allows us to use this image in a Docker-Compose
    environment that can wait on files to be built. For instance, building
    expensive .apk files for Android projects to be pushed to S3.

--- start.sh
+++ start.sh
@@ -5,23 +5,46 @@ set -e
 : ${ACCESS_KEY:?"ACCESS_KEY env variable is required"}
 : ${SECRET_KEY:?"SECRET_KEY env variable is required"}
 : ${S3_PATH:?"S3_PATH env variable is required"}
-: ${GIT_REPO:?"GIT_REPO env variable is required"}
 
 export DATA_PATH=${DATA_PATH:-/data/}
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 
-rm -rf /data
-git clone $GIT_REPO /data
-rm -rf /data/.git
-
 echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
 
-if [[ "$1" == 'no-cron' ]]; then
+echo "Parameter is $1"
+
+if [[ "$1" == 'server' ]]; then
+    echo "Starting server..."
+    exec /server.py
+
+elif [[ "$1" == 'git' ]]; then
+    echo "Backing up git repos..."
+    : ${GIT_REPO:?"GIT_REPO env variable is required"}
+    rm -rf $DATA_PATH
+    git clone $GIT_REPO $DATA_PATH
+    rm -rf $DATA_PATH/.git
     exec /sync.sh
+
+elif [[ "$1" == 'wait-sync' ]]; then
+    echo "Will wait until files are at $DATA_PATH..."
+    inotifywait $DATA_PATH -e create -e moved_to |
+      while read path action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+        # do something with the file
+      done
+    exec /sync.sh
+
+elif [[ "$1" == 'no-cron' ]]; then
+    echo "Sync the directory $DATA_DIR..."
+    exec /sync.sh
+
 elif [[ "$1" == 'delete' ]]; then
+    echo "Delete files from $S3_PATH"
     exec /usr/local/bin/s3cmd del -r "$S3_PATH"
+
 else
+    echo "Executing Cron..."
     LOGFIFO='/var/log/cron.fifo'
     if [[ ! -e "$LOGFIFO" ]]; then
         mkfifo "$LOGFIFO"
