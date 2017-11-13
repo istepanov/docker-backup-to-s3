8fc30cc22494801c2737b2d7b1ed413054ec7bf1
Pedro Graber <pgraber@usnews.com>
2017-11-13 13:10:40 -0500

    Saving tar in flat structure for ease of restoring

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ echo "Job started: $(date)"
 
 cd /
 rm -f backup.tar.gz 
-tar --exclude='*.git*' --exclude='*.tar.gz' -czf backup.tar.gz ${DATA_PATH}* ${DATA_PATH}.??*
+tar --exclude='*.git*' --exclude='*.tar.gz' -czf ../backup.tar.gz ${DATA_PATH}* ${DATA_PATH}.??*
 
 /usr/local/bin/s3cmd sync $PARAMS "/backup.tar.gz" "$S3_PATH"
 
